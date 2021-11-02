5bd4efd116be367585938b900e70dbe309ce0e49
roxieetc <roxieschnoor@yahoo.com>
2021-11-01 19:31:29 -0700

    refactored code

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -4,18 +4,18 @@ import { Link } from 'react-router-dom';
 function Homepage() {
 
     return (
-        <div>
+        <div className="buttonList">
         <h1>Valorant by Riot Games</h1>
-        <Link to ={'/agents'} className="agentsLink">
+        <Link to ={'/agents'} className="singleLink">
             <button className="hpButton"><h1 className="linkText">agents</h1></button>
         </Link>
-        <Link to ={'/maps'} className="mapsLink">
+        <Link to ={'/maps'} className="singleLink">
             <button className="hpButton"><h1 className="linkText">maps</h1></button>
         </Link>
-        <Link to ={'/weapons'} className="weaponsLink">
+        <Link to ={'/weapons'} className="singleLink">
             <button className="hpButton"><h1 className="linkText">weapons</h1></button>
         </Link>
-        <Link to ={'/playercards'} className="cardsLink">
+        <Link to ={'/playercards'} className="singleLink">
             <button className="hpButton"><h1 className="linkText">player cards</h1></button>
         </Link>
         </div>
