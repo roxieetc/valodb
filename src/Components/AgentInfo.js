aec33216006b5c7a0d8686fdd1b82e3651809b08
roxieetc <roxieschnoor@yahoo.com>
2021-11-04 22:50:47 -0700

    added agent roles

--- src/Components/AgentInfo.js
+++ src/Components/AgentInfo.js
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { isCompositeComponent } from "react-dom/test-utils";
 
 function AgentInfo({ match }) {
 
     const [info, setInfo] = useState([]);
     const [abilities, setAbilities] = useState([]);
+    const [roles, setRoles] = useState([]);
 
     const getInfo = async () => {
         const response = await fetch(`https://valorant-api.com/v1/agents/${match.params.id}`)
         const json = await response.json()
         setInfo(json.data)
         setAbilities(json.data.abilities)
+        setRoles(json.data.role)
     }
 
     useEffect(() => {
@@ -20,6 +21,7 @@ function AgentInfo({ match }) {
 
     let abilitiesList = abilities.map((util) => {
         const {displayName, displayIcon} = util
+
         return (
             <div className="abilityList">
                 <img src={displayIcon} className="abilityIcon" alt="ability" />
@@ -37,6 +39,9 @@ function AgentInfo({ match }) {
                     </div>
                     <div className="descriptionContainer">
                     <h1>{info.displayName}'s Info</h1>
+                    <div className="roleContainer">
+                    <h3><img src={roles.displayIcon} className="agentRoleIcon" alt="role" />{roles.displayName}</h3>
+                        </div>
                         <p className="agentDescription">{info.description}</p>
                         <h2>Abilities</h2>
                         {abilitiesList}
