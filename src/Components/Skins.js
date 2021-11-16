a9f0943261651cc100ee2385a82bed7719710f63
roxieetc <roxieschnoor@yahoo.com>
2021-11-16 15:22:55 -0800

    fixed broken skin images

--- src/Components/Skins.js
+++ src/Components/Skins.js
@@ -8,21 +8,19 @@ function Skins({ match }) {
         const response = await fetch(`https://valorant-api.com/v1/weapons/${match.params.id}`)
         const json = await response.json()
         setSkins(json.data.skins)
-        console.log(json.data.skins)
     }
 
     useEffect(() => {
         getSkins()
     }, [])
 
-    let skinsList = skins.map((info) =>{
-        const {displayName, displayIcon} = info
-    
+
+    let skinsList = skins.map((info) => {
 
     return (
         <div className="skinCards">
-        <h2 className="skinNames">{displayName}</h2>
-        <img src={displayIcon} className="skinImages" alt="weapon" />
+        <h2 className="skinNames">{info.displayName}</h2>
+        <img src={info.chromas[0].fullRender} className="skinImages" alt="weapon" />
         </div>
     )
 })
