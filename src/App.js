a2e378e02cbf3fc45a191ab95a9a9ca22ab86cfd
roxieetc <roxieschnoor@yahoo.com>
2021-11-16 16:24:53 -0800

    added homepage link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import AgentList from './Components/AgentList';
 import Homepage from './Components/Homepage';
 import MapList from './Components/MapList';
@@ -8,12 +8,18 @@ import PlayerCards from './Components/PlayerCards';
 import AgentInfo from './Components/AgentInfo';
 import Skins from './Components/Skins';
 import Sprays from './Components/Sprays';
-import Bundles from './Components/BundlesList'
+import Bundles from './Components/BundlesList';
+import Icon from './valologo.png';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <div className="homeLinkContainer">
+        <Link to={'/'}>
+        <img src={Icon} alt="logo" className="homeLink"/>
+        </Link>
+        </div>
         <Route exact path='/' component={Homepage} />
         <Route exact path ='/agents' component={AgentList} />
         <Route exact path='/maps' component={MapList} />
